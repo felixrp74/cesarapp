c01253db5c279af14d4c5aef910cd12d5eaef668
felixrp74 <felixrp74@gmail.com>
2022-08-05 15:04:30 -0500

    first commit

---
+++ app/src/main/java/com/example/cesarapp/CourseAdapter.kt
@@ -0,0 +1,13 @@
+package com.example.cesarapp
+
+import android.view.View
+import androidx.recyclerview.widget.RecyclerView
+
+class CourseAdapter {
+
+    inner class CourseViewHolder(view:View): RecyclerView.ViewHolder(view){
+
+    }
+
+
+}
---
+++ app/src/main/java/com/example/cesarapp/CourseEntity.kt
@@ -0,0 +1,14 @@
+package com.example.cesarapp
+
+import android.provider.ContactsContract
+
+data class CourseEntity (
+    val id:Int,
+    val nickname:String?,
+    val name:String,
+    val mode:String?,
+    val startdate:String?,
+    val desc:String?,
+    val imagen:String
+
+        )
